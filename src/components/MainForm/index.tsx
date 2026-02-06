407e7e6955553d18594a6ad22eb8d907d2966a60
Edvaldo Fendi <edvaldo.defendi@gmail.com>
2026-02-06 11:24:32 -0300

    feat: adiciona utils e config de ciclos

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -2,18 +2,68 @@ import { PlayCircleIcon } from 'lucide-react';
 import { Button } from '../Button';
 import { Cycles } from '../Cycles';
 import { Input } from '../Input';
+import { useRef } from 'react';
+import type { TaskModel } from '../../models/TaskModel';
+import { useTaskContext } from '../../contexts/TaskContext/useTaskContext';
+import { getNextCycle } from '../../utils/getNextCycle';
+import { getNextCycleType } from '../../utils/getNextCycleType';
+import { formatSecondsToMinutes } from '../../utils/formatSecondsToMinutes';
 
 export function MainForm() {
+  const { state, setState } = useTaskContext();
+  const taskNameInput = useRef<HTMLInputElement>(null);
+
+  const nextCycle = getNextCycle(state.currentCycle);
+  const nextCycleType = getNextCycleType(nextCycle);
+
+  function handleSubmit(
+    event: React.SyntheticEvent<HTMLFormElement, SubmitEvent>,
+  ) {
+    event.preventDefault();
+    if (taskNameInput.current === null || taskNameInput.current.value === '') {
+      return;
+    }
+
+    const taskName = taskNameInput.current.value.trim();
+
+    if (!taskName) {
+      alert('Por favor, insira o nome da tarefa válida.');
+      return;
+    }
+
+    const newTask: TaskModel = {
+      id: String(Date.now()),
+      name: taskName,
+      startDate: Date.now(),
+      completedDate: null,
+      interruptedDate: null,
+      duration: state.config[nextCycleType],
+      type: nextCycleType,
+    };
+
+    const secondsRemaining = newTask.duration * 60;
+
+    setState(prevState => {
+      return {
+        ...prevState,
+        activeTask: newTask,
+        currentCycle: nextCycle,
+        secondsRemaining: secondsRemaining,
+        formattedSecondsRemaining: formatSecondsToMinutes(secondsRemaining),
+        tasks: [...prevState.tasks, newTask],
+      };
+    });
+  }
+
   return (
-    <form className='form' action=''>
+    <form onSubmit={handleSubmit} className='form' action=''>
       <div className='formRow'>
         <Input
           type='text'
           label='Task'
           id='input'
           placeholder='Digite uma tarefa'
-          disabled
-          defaultValue='Estudar React'
+          ref={taskNameInput}
         />
       </div>
 
