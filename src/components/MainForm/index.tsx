61e76b743c571c2ac3b10da4301f09b5b625cee2
Edvaldo Fendi <edvaldo.defendi@gmail.com>
2026-02-09 16:08:35 -0300

    feat: persist task state and reuse last task name

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -14,6 +14,7 @@ import { showMessage } from '../../adapters/showMessage';
 export function MainForm() {
   const { state, dispatch } = useTaskContext();
   const taskNameInput = useRef<HTMLInputElement>(null);
+  const lastTaskName = state.tasks[state.tasks.length - 1]?.name ?? '';
 
   const nextCycle = getNextCycle(state.currentCycle);
   const nextCycleType = getNextCycleType(nextCycle);
@@ -50,8 +51,8 @@ export function MainForm() {
   }
 
   function handleInterrupt() {
-    showMessage.dismiss()
-    showMessage.error("Tarefa interrompida.")
+    showMessage.dismiss();
+    showMessage.error('Tarefa interrompida.');
     dispatch({
       type: ETaskActionsTypes.INTERRUPT_TASK,
     });
@@ -67,6 +68,7 @@ export function MainForm() {
           placeholder='Digite uma tarefa'
           ref={taskNameInput}
           disabled={!!state.activeTask}
+          defaultValue={lastTaskName}
         />
       </div>
 
